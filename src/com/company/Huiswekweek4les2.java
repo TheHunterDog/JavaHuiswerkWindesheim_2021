74688f8452ba881fb62acd50be267158f7c9fb3e
Mark Heijnekamp <mark@heijnekamp.nl>
2021-03-12 12:48:16 +0100

    Huiswekweek4les2.java

---
+++ src/com/company/Huiswekweek4les2.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class Huiswekweek4les2 {
+
+    public static void main(String[] args) {
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter("C:/bestandsnaam.txt");
+            String[] namen = new String[]{"Michiel", "Amy", "Joost"};
+            for (int i = 0; i < 4; i++) {
+                writer.println(namen[i]);
+            }
+
+            writer.println("hoofdstuk 1");
+            writer.close();
+
+        }
+        catch (FileNotFoundException e){
+            System.out.println("Bestand kan niet gemaakt worden:");
+            System.out.println(e.getMessage());
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Er is iets mis gegaan");
+            System.out.println(e.getMessage());
+        }
+        finally {
+            writer.close();
+
+        }
+
+
+    }
+}
